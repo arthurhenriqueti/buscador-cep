e6382c901a345c6e87928b0e33c445471bf6fff0
Arthur Henrique <arthurbleckrep2@gmail.com>
2022-07-20 09:10:12 -0300

    api added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,34 @@
+import { useState } from "react";
 import { FiSearch } from "react-icons/fi";
+import axios from "axios";
 import Header from "../components/Header";
 import "./Home.scss";
 
 const Home = () => {
+    const [input, setInput] = useState("");
+    const [resultInput, setResultInput] = useState("");
+
+    const showInfo = () => {
+        const element = document.getElementsByClassName("home-cep-area")[0];
+
+        if (element.style.display == "flex") {
+            element.style.display = "none";
+        } else {
+            element.style.display = "flex";
+        }
+    };
+
+    const searchCep = async () => {
+        await axios
+            .get(`https://viacep.com.br/ws/${input}/json/`)
+            .then((response) => {
+                setResultInput(response.data);
+            });
+        showInfo();
+
+        console.log(resultInput);
+    };
+
     return (
         <>
             <Header />
@@ -14,17 +40,20 @@ const Home = () => {
                             type="text"
                             placeholder="Digite seu cep..."
                             required
+                            onChange={(e) => {
+                                setInput(e.target.value);
+                            }}
                         />
-                        <button className="home-btn-search">
+                        <button className="home-btn-search" onClick={searchCep}>
                             <FiSearch size={25} color="#fff" />
                         </button>
                     </div>
                     <div className="home-cep-area">
-                        <h2>CEP</h2>
-                        <span>Logradouro</span>
-                        <span>Complemento</span>
-                        <span>Bairro</span>
-                        <span>Estado</span>
+                        <h2>{resultInput.cep}</h2>
+                        <span>{resultInput.logradouro}</span>
+                        <span>{resultInput.bairro}</span>
+                        <span>{resultInput.localidade}</span>
+                        <span>{resultInput.uf}</span>
                     </div>
                 </section>
             </main>
